6c78f48ef3b3e01582eb25ed6ed3a7eeb3af54de
nguyenanhvu03 <nguyenanhvu.tdc2223@gmail.com>
2024-04-11 15:26:41 +0700

    first commit

---
+++ src/Example/MyComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+
+    render() {
+        return (
+            <div>Heloo my component </div>
+        )
+    }
+
+}
+
+export default MyComponent;
